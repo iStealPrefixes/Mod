6b82225660036c7df7ffe33328d99416d70ee9b2
Blank256 <Blanks.house1@gmail.com>
2021-02-22 22:06:43 +0000

    dont use the block I made for the love of god, I beg of you, DONT USE THE BLOCK I MADE

---
+++ src/main/java/net/mcreator/spoi/procedures/ButtonclickppnoProcedure.java
@@ -0,0 +1,66 @@
+package net.mcreator.spoi.procedures;
+
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.spoi.SpoiModElements;
+
+import java.util.Map;
+
+@SpoiModElements.ModElement.Tag
+public class ButtonclickppnoProcedure extends SpoiModElements.ModElement {
+	public ButtonclickppnoProcedure(SpoiModElements instance) {
+		super(instance, 6);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure Buttonclickppno!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure Buttonclickppno!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure Buttonclickppno!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure Buttonclickppno!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure Buttonclickppno!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		for (int index0 = 0; index0 < (int) (5); index0++) {
+			if (!world.getWorld().isRemote) {
+				world.playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("spoi:calming")),
+						SoundCategory.NEUTRAL, (float) 999, (float) 1);
+			} else {
+				world.getWorld().playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("spoi:calming")),
+						SoundCategory.NEUTRAL, (float) 999, (float) 1, false);
+			}
+			entity.extinguish();
+			entity.setFire((int) 1);
+		}
+	}
+}
