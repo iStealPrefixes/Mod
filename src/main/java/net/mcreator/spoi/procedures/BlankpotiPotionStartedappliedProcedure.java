3c18f0cf990863f54d9c7e2eb59e9ff3ef4329da
iStealPrefixes <unconfigured@null.spigotmc.org>
2021-02-22 22:14:13 +0000

    i am commited

---
+++ src/main/java/net/mcreator/spoi/procedures/BlankpotiPotionStartedappliedProcedure.java
@@ -0,0 +1,35 @@
+package net.mcreator.spoi.procedures;
+
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.spoi.SpoiModElements;
+
+import java.util.Map;
+
+@SpoiModElements.ModElement.Tag
+public class BlankpotiPotionStartedappliedProcedure extends SpoiModElements.ModElement {
+	public BlankpotiPotionStartedappliedProcedure(SpoiModElements instance) {
+		super(instance, 8);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure BlankpotiPotionStartedapplied!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		String xuwu = "";
+		String full = "";
+		full = (String) ((entity) + "" + ("is now gay"));
+		{
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().sendMessage(new StringTextComponent((full)));
+		}
+	}
+}
